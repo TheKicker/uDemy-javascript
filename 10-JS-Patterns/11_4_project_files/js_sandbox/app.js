56e95e924c3d343242e9a9fa0f226c467589f7a7
The Kicker <cav.lemasters@geneva.edu>
2020-03-16 13:21:29 -0400

    New Project - Calorie Tracker

--- 10-JS-Patterns/11_4_project_files/js_sandbox/app.js
+++ 10-JS-Patterns/11_4_project_files/js_sandbox/app.js
@@ -1,39 +1,41 @@
-function MemberFactory() {
-  this.createMember = function(name, type) {
-    let member;
+function MemberFactory () {
+	this.createMember = function (name, type) {
+		let member;
 
-    if(type === 'simple') {
-      member = new SimpleMembership(name);
-    } else if (type === 'standard') {
-      member = new StandardMembership(name);
-    } else if (type === 'super') {
-      member = new SuperMembership(name);
-    }
+		if (type === 'simple') {
+			member = new SimpleMembership(name);
+		}
+		else if (type === 'standard') {
+			member = new StandardMembership(name);
+		}
+		else if (type === 'super') {
+			member = new SuperMembership(name);
+		}
 
-    member.type = type;
+		member.type = type;
 
-    member.define =  function() {
-      console.log(`${this.name} (${this.type}): ${this.cost}`);
-    }
+		member.define = function () {
+			console.log(`${this.name} (${this.type}): ${this.cost}`);
+		};
 
-    return member;
-  }
+		return member;
+	};
 }
 
-const SimpleMembership = function(name) {
-  this.name = name;
-  this.cost = '$5';
-}
+const SimpleMembership = function (name) {
+	this.name = name;
+	this.cost = '$5';
+};
 
-const StandardMembership = function(name) {
-  this.name = name;
-  this.cost = '$15';
-}
+const StandardMembership = function (name) {
+	this.name = name;
+	this.cost = '$15';
+};
 
-const SuperMembership = function(name) {
-  this.name = name;
-  this.cost = '$25';
-}
+const SuperMembership = function (name) {
+	this.name = name;
+	this.cost = '$25';
+};
 
 const members = [];
 const factory = new MemberFactory();
@@ -42,9 +44,12 @@ members.push(factory.createMember('John Doe', 'simple'));
 members.push(factory.createMember('Chris Jackson', 'super'));
 members.push(factory.createMember('Janice Williams', 'simple'));
 members.push(factory.createMember('Tom Smith', 'standard'));
+members.push(factory.createMember('Enid Rhee', 'super'));
+members.push(factory.createMember('Jason Valroza', 'standard'));
+members.push(factory.createMember('Carl Lavane', 'simple'));
+members.push(factory.createMember('Thomas Haverford', 'simple'));
+members.push(factory.createMember('William Rust', 'super'));
 
-// console.log(members);
-
-members.forEach(function(member) {
-  member.define();
+members.forEach(function (member) {
+	member.define();
 });
